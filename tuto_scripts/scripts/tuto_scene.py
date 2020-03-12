2edbe79545958a82a095569f7318957cd065e002
Lunabot87 <rkwoskek@naver.com>
2020-03-12 21:25:44 +0900

    table update

--- tuto_scripts/scripts/tuto_scene.py
+++ tuto_scripts/scripts/tuto_scene.py
@@ -9,29 +9,8 @@ import geometry_msgs.msg
 from math import pi
 from std_msgs.msg import String
 from moveit_commander.conversions import pose_to_list
-## END_SUB_TUTORIAL
 
-def all_close(goal, actual, tolerance):
-  """
-  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
-  @param: goal       A list of floats, a Pose or a PoseStamped
-  @param: actual     A list of floats, a Pose or a PoseStamped
-  @param: tolerance  A float
-  @returns: bool
-  """
-  all_equal = True
-  if type(goal) is list:
-    for index in range(len(goal)):
-      if abs(actual[index] - goal[index]) > tolerance:
-        return False
 
-  elif type(goal) is geometry_msgs.msg.PoseStamped:
-    return all_close(goal.pose, actual.pose, tolerance)
-
-  elif type(goal) is geometry_msgs.msg.Pose:
-    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
-
-  return True
 
 class MoveGroupPythonIntefaceTutorial(object):
   """MoveGroupPythonIntefaceTutorial"""
@@ -42,55 +21,12 @@ class MoveGroupPythonIntefaceTutorial(object):
     rospy.init_node('move_group_python_interface_tutorial',
                     anonymous=True)
 
-    robot = moveit_commander.RobotCommander()
-
     scene = moveit_commander.PlanningSceneInterface()
 
-    group_name = "manipulator"
-    group = moveit_commander.MoveGroupCommander(group_name)
-
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path',
-                                                   moveit_msgs.msg.DisplayTrajectory,
-                                                   queue_size=20)
-
-
-    planning_frame = group.get_planning_frame()
-
-    eef_link = group.get_end_effector_link()
-
-    group_names = robot.get_group_names()
 
     self.box_name = ''
     self.table_name = ''
-    self.robot = robot
     self.scene = scene
-    self.group = group
-    self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
-
-
-
-  def wait_for_state_update(self, box_is_known=False, box_is_attached=False, timeout=4):
-    box_name = self.box_name
-    table_name = self.table_name
-    scene = self.scene
-
-    start = rospy.get_time()
-    seconds = rospy.get_time()
-    while (seconds - start < timeout) and not rospy.is_shutdown():
-      attached_objects = scene.get_attached_objects([box_name])
-      attached_objects = scene.get_attached_objects([table_name])
-
-      is_known = box_name in scene.get_known_object_names()
-      is_known = table_name in scene.get_known_object_names()
-
-
-      rospy.sleep(0.1)
-      seconds = rospy.get_time()
-
-    return False
 
 
   def add_box(self, timeout=4):
@@ -101,16 +37,18 @@ class MoveGroupPythonIntefaceTutorial(object):
 
 
     box_pose = geometry_msgs.msg.PoseStamped()
-    box_pose.header.frame_id = "ee_link"
+    box_pose.header.frame_id = "world"
     box_pose.pose.orientation.w = 1.0
+    box_pose.pose.position.x = -0.6
     box_name = "box"
-    scene.add_box(box_name, box_pose, size=(0.1, 0.1, 0.1))
+    scene.add_box(box_name, box_pose, size=(0.3, 0.5, 0.5))
 
     table_pose = geometry_msgs.msg.PoseStamped()
     table_pose.header.frame_id = "world"
     table_pose.pose.orientation.w = 1.0
+    table_pose.pose.position.y = 0.6
     table_name = "table"
-    scene.add_box(table_name, table_pose, size=(0.2, 0.2, 0.2))
+    scene.add_box(table_name, table_pose, size=(0.5, 0.3, 0.5))
 
     self.box_name=box_name
     self.table_name=table_name
